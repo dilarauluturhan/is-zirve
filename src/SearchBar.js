9b1a2c1ff9157efb5f1cd6175e6b19a3f4df1b8d
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-07-25 14:20:47 +0300

    feat: db filtered

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import JSONJOBS from './db.json';
 
 function SearchBar() {
+    const [searchTerm, setSearchTerm] = useState('')
+
     return (
         <>
             <header>
                 <img className='logoImg' src='./img/iszirve.png' />
             </header>
             <main>
-                <input className='searchInput' type='text' placeholder='İş bul...' />
-                {JSONJOBS.map((val, i) => {
+                <input
+                    className='searchInput'
+                    type='text'
+                    placeholder='İş bul...'
+                    onChange={e => { setSearchTerm(e.target.value) }}
+                />
+                {JSONJOBS.filter((val) => {
+                    if (searchTerm == "") {
+                        return val
+                    } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+                        return val
+                    }
+                }).map((val, i) => {
                     return (
-                        <div>
-                            <p className='searchResult'>{val.title}</p>
+                        <div key={i}>
+                            <ul>
+                                <li className='resultTitle'>{val.title}</li>
+                                <li className='resultCity'>{val.city}</li>
+                            </ul>
                         </div>
                     )
                 })}
-                <button className='searchButton'>Ara</button>
+                <button className='searchButton'>
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </button>
             </main>
         </>
     )
