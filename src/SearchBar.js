a12c3bce4e76d05383640a6a39f7a36cbfce4503
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-07-25 18:14:24 +0300

    feat: updated map and filter

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,39 +2,55 @@ import React, { useState } from 'react';
 import JSONJOBS from './db.json';
 
 function SearchBar() {
-    const [searchTerm, setSearchTerm] = useState('')
+    const [value, setValue] = useState('');
 
+    const onChange = (e) => {
+        setValue(e.target.value);
+    }
+
+    const onSearch = (searchTerm) => {
+        console.log('Search', searchTerm);
+        setValue(searchTerm); // çıkan meskeğe tıkladığımda input'ta aradığım kelimeyi tamamlıyor
+    }
     return (
         <>
             <header>
                 <img className='logoImg' src='./img/iszirve.png' />
             </header>
             <main>
-                <input
-                    className='searchInput'
-                    type='text'
-                    placeholder='İş bul...'
-                    onChange={e => { setSearchTerm(e.target.value) }}
-                />
-                {JSONJOBS.filter((val) => {
-                    if (searchTerm == "") {
-                        return val
-                    } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-                        return val
-                    }
-                }).map((val, i) => {
-                    return (
-                        <div key={i}>
-                            <ul>
-                                <li className='resultTitle'>{val.title}</li>
-                                <li className='resultCity'>{val.city}</li>
-                            </ul>
-                        </div>
-                    )
-                })}
-                <button className='searchButton'>
-                    <i className="fa-solid fa-magnifying-glass"></i>
-                </button>
+                <div>
+                    <div>
+                        <input
+                            type='text'
+                            value={value}
+                            onChange={onChange}
+                        />
+                        <button onClick={() => onSearch(value)}>
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </div>
+                    <div>
+                        {JSONJOBS.filter(jobs => {
+                            const searchTerm = value.toLowerCase();
+                            const title = jobs.title.toLowerCase();
+
+                            return searchTerm && title.startsWith(searchTerm) && title !== searchTerm;
+                        })
+                            .slice(0, 3) // 3 tane listelesin
+                            .map((jobs) => (
+                                <div
+                                    onClick={() => onSearch(jobs.title)}
+                                    key={jobs.id}
+                                >
+                                    <ul>
+                                        <li>{jobs.title}</li>
+                                        <li>{jobs.city}</li>
+                                    </ul>
+                                </div>
+                            ))}
+                        <a href='#\'>Daha Fazla...</a>
+                    </div>
+                </div>
             </main>
         </>
     )
