150655a22c203d4fdefdff8f6458c7bdd71f4323
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-07-26 13:08:16 +0300

    style: added SearchBar style

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 <div align="center">
   <h1 align="center">İŞ ZİRVE</h1>
 </div>
-
-## About The Project✨
-![](https://github.com/dilarauluturhan/is-zirve/assets/120499369/ba65847f-7d6c-459e-b175-a2219236b5d2)
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -13,19 +13,63 @@ function SearchBar() {
         setValue(searchTerm); // çıkan meskeğe tıkladığımda input'ta aradığım kelimeyi tamamlıyor
     }
     return (
-        <>
+        <div>
             <header>
-                <img className='logoImg' src='./img/iszirve.png' />
+                
+                    <div class="flex-flex flex-direction--column" style={{ minHeight: "100vh", flexGrow: "1" }}>
+                        <div class="loginbackground box-background--white padding-top--64">
+                            <div class="loginbackground-gridContainer">
+                                <div class=" flex-flex" style={{ gridArea: "top / start / 8 / end" }}>
+                                    <div
+                                        style={{ backgroundImage: "linear-gradient(white 0%, rgb(247, 250, 252) 33%)", flexGrow: "1" }}>
+                                    </div>
+                                </div>
+                                <div class="flex-flex" style={{ gridArea: "4 / 2 / auto / 5" }}>
+                                    <div class="box-divider--light-all-2 animationLeftRight tans3s" style={{ flexGrow: "1" }}>
+                                    </div>
+                                </div>
+                                <div class="flex-flex" style={{ gridArea: "6 / start / auto / 2" }}>
+                                    <div class="box-background--blue800" style={{ flexGrow: "1" }}></div>
+                                </div>
+                                <div class="flex-flex" style={{ gridArea: "7 / start / auto / 4" }}>
+                                    <div class="box-background--blue animationLeftRight" style={{ flexGrow: "1" }}></div>
+                                </div>
+                                <div class="flex-flex" style={{ gridArea: "8 / 4 / auto / 6" }}>
+                                    <div class="box-background--gray100 animationLeftRight tans3s" style={{ flexGrow: "1" }}>
+                                    </div>
+                                </div>
+                                <div class="flex-flex" style={{ gridArea: "2 / 15 / auto / end" }}>
+                                    <div class="box-background--blue800 animationRightLeft tans4s" style={{ flexGrow: "1" }}>
+                                    </div>
+                                </div>
+                                <div class="flex-flex" style={{ gridArea: "3 / 14 / auto / end" }}>
+                                    <div class="box-background--blue animationRightLeft" style={{ flexGrow: "1" }}></div>
+                                </div>
+                                <div class="flex-flex" style={{ gridArea: "4 / 17 / auto / 20" }}>
+                                    <div class="box-background--gray100 animationRightLeft tans4s" style={{ flexGrow: "1" }}>
+                                    </div>
+                                </div>
+                                <div class="flex-flex" style={{ gridArea: "5 / 14 / auto / 17" }}>
+                                    <div class="box-divider--light-all-2 animationRightLeft tans3s" style={{ flexGrow: "1" }}>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
             </header>
-            <main>
-                <div>
-                    <div>
+            <main className='login-root'>
+                <div className='loginbackground'>
+                <h1 className='header'>işZirve</h1>
+                    <div className='search'>
                         <input
                             type='text'
+                            className='search-input'
                             value={value}
                             onChange={onChange}
+                            placeholder='İş bul...'
                         />
-                        <button onClick={() => onSearch(value)}>
+                        <button onClick={() => onSearch(value)} className='search-button'>
                             <i className="fa-solid fa-magnifying-glass"></i>
                         </button>
                     </div>
@@ -42,17 +86,17 @@ function SearchBar() {
                                     onClick={() => onSearch(jobs.title)}
                                     key={jobs.id}
                                 >
-                                    <ul>
+                                    <ul className='jobList'>
                                         <li>{jobs.title}</li>
                                         <li>{jobs.city}</li>
                                     </ul>
                                 </div>
                             ))}
-                        <a href='#\'>Daha Fazla...</a>
+                        <a className='moreJobs' href='#\'>Daha fazla...</a>
                     </div>
                 </div>
             </main>
-        </>
+        </div>
     )
 }
 
