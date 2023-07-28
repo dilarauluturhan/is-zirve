01268cf61c63827ae4b3093af0ad2348a25abfd4
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-07-28 23:25:10 +0300

    feat: added filter feature

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -5,20 +5,46 @@ import Pagination from './Pagination';
 
 function JobList() {
 
-  const jobsPerPage = 5; // her sayfada kaç iş ilanı gösterileceği
+  const jobsPerPage = 7; // her sayfada kaç iş ilanı gösterileceği
   const [currentPage, setCurrentPage] = useState(1); // şu anki sayfa numarası
+  const [filter, setFilter] = useState(''); // filtreleme işlemi için
+
+  // iş ilanlarını filtreleyen fonksiyon
+  const filterJobs = (jobList) => {
+    switch (filter) {
+      case 'title':
+        return jobList.sort((a, b) => a.title.localeCompare(b.title));
+      case 'new':
+        return jobList.sort((a, b) => new Date(b.date) - new Date(a.date));
+      case 'old':
+        return jobList.sort((a, b) => new Date(a.date) - new Date(b.date));
+      default:
+        return jobList;
+    }
+  }
+
+  // iş ilanlarını filtreleyerek şu anki sayfadaki iş ilanlarını almak için
+  const currentJobs = filterJobs(JSONJOBS).slice(
+    (currentPage - 1) * jobsPerPage,
+    currentPage * jobsPerPage
+  );
+
+  // filtreyi sıfırlayan fonksiyon
+  const resetFilter = () => {
+    setFilter(''); // filtreyi sıfırla
+    setCurrentPage(1); // sayfa numarasını sıfırla
+  }
 
   // toplam sayfa sayısı hesabı
   const totalPages = Math.ceil(JSONJOBS.length / jobsPerPage);
 
-  // şu anki sayfadaki iş ilanları
-  const currentJobs = JSONJOBS.slice(
-    (currentPage -1 ) * jobsPerPage,
-    currentPage * jobsPerPage
-  );
-
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
+  }
+
+  const handleFilterChange = (selectedFilter) => {
+    setFilter(selectedFilter);
+    setCurrentPage(1); //filtre değiştiğinde sayfa numarasını sıfırla
   }
 
   return (
@@ -34,9 +60,10 @@ function JobList() {
             Filtrele
           </button>
           <div className='dropdown-content'>
-            <a href='#\'>İsme göre</a>
-            <a href='#\'>Tarihe göre en yeni</a>
-            <a href='#\'>Tarihe göre en eski</a>
+            <a href='#\' onClick={() => handleFilterChange('title')}>İsme göre</a>
+            <a href='#\' onClick={() => handleFilterChange('new')}>Tarihe göre en yeni</a>
+            <a href='#\' onClick={() => handleFilterChange('old')}>Tarihe göre en eski</a>
+            <a href='#\' onClick={resetFilter}>Filtreyi sıfırla</a>
           </div>
         </div>
       </header>
@@ -55,9 +82,9 @@ function JobList() {
       </main>
       <footer>
         <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={handlePageChange}
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
         />
       </footer>
     </div>
