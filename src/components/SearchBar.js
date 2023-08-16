b9eff54da9a8f21049b8710766beb939b5d54cca
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-08-16 19:35:42 +0300

    style: searchbar mobile responsive

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -28,41 +28,41 @@ function SearchBar() {
     return (
         <div className='search-bar'>
             <header>
-                <div class="flex-flex flex-direction--column" style={{ minHeight: "100vh", flexGrow: "1" }}>
-                    <div class="loginbackground box-background--white padding-top--64">
-                        <div class="loginbackground-gridContainer">
-                            <div class=" flex-flex" style={{ gridArea: "top / start / 8 / end" }}>
+                <div className="flex-flex flex-direction--column" style={{ minHeight: "100vh", flexGrow: "1" }}>
+                    <div className="loginbackground box-background--white padding-top--64">
+                        <div className="loginbackground-gridContainer">
+                            <div className=" flex-flex" style={{ gridArea: "top / start / 8 / end" }}>
                                 <div
                                     style={{ backgroundImage: "linear-gradient(white 0%, rgb(247, 250, 252) 33%)", flexGrow: "1" }}>
                                 </div>
                             </div>
-                            <div class="flex-flex" style={{ gridArea: "4 / 2 / auto / 5" }}>
-                                <div class="box-divider--light-all-2 animationLeftRight tans3s" style={{ flexGrow: "1" }}>
+                            <div className="flex-flex" style={{ gridArea: "4 / 2 / auto / 5" }}>
+                                <div className="box-divider--light-all-2 animationLeftRight tans3s" style={{ flexGrow: "1" }}>
                                 </div>
                             </div>
-                            <div class="flex-flex" style={{ gridArea: "6 / start / auto / 2" }}>
-                                <div class="box-background--blue800" style={{ flexGrow: "1" }}></div>
+                            <div className="flex-flex" style={{ gridArea: "6 / start / auto / 2" }}>
+                                <div className="box-background--blue800" style={{ flexGrow: "1" }}></div>
                             </div>
-                            <div class="flex-flex" style={{ gridArea: "7 / start / auto / 4" }}>
-                                <div class="box-background--blue animationLeftRight" style={{ flexGrow: "1" }}></div>
+                            <div className="flex-flex" style={{ gridArea: "7 / start / auto / 4" }}>
+                                <div className="box-background--blue animationLeftRight" style={{ flexGrow: "1" }}></div>
                             </div>
-                            <div class="flex-flex" style={{ gridArea: "8 / 4 / auto / 6" }}>
-                                <div class="box-background--gray100 animationLeftRight tans3s" style={{ flexGrow: "1" }}>
+                            <div className="flex-flex" style={{ gridArea: "8 / 4 / auto / 6" }}>
+                                <div className="box-background--gray100 animationLeftRight tans3s" style={{ flexGrow: "1" }}>
                                 </div>
                             </div>
-                            <div class="flex-flex" style={{ gridArea: "2 / 15 / auto / end" }}>
-                                <div class="box-background--blue800 animationRightLeft tans4s" style={{ flexGrow: "1" }}>
+                            <div className="flex-flex" style={{ gridArea: "2 / 15 / auto / end" }}>
+                                <div className="box-background--blue800 animationRightLeft tans4s" style={{ flexGrow: "1" }}>
                                 </div>
                             </div>
-                            <div class="flex-flex" style={{ gridArea: "3 / 14 / auto / end" }}>
-                                <div class="box-background--blue animationRightLeft" style={{ flexGrow: "1" }}></div>
+                            <div className="flex-flex" style={{ gridArea: "3 / 14 / auto / end" }}>
+                                <div className="box-background--blue animationRightLeft" style={{ flexGrow: "1" }}></div>
                             </div>
-                            <div class="flex-flex" style={{ gridArea: "4 / 17 / auto / 20" }}>
-                                <div class="box-background--gray100 animationRightLeft tans4s" style={{ flexGrow: "1" }}>
+                            <div className="flex-flex" style={{ gridArea: "4 / 17 / auto / 20" }}>
+                                <div className="box-background--gray100 animationRightLeft tans4s" style={{ flexGrow: "1" }}>
                                 </div>
                             </div>
-                            <div class="flex-flex" style={{ gridArea: "5 / 14 / auto / 17" }}>
-                                <div class="box-divider--light-all-2 animationRightLeft tans3s" style={{ flexGrow: "1" }}>
+                            <div className="flex-flex" style={{ gridArea: "5 / 14 / auto / 17" }}>
+                                <div className="box-divider--light-all-2 animationRightLeft tans3s" style={{ flexGrow: "1" }}>
                                 </div>
                             </div>
                         </div>
